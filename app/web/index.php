c35a0f5fea5a1516abdf51e63559db926fb2ebec
Muhammad Ilham Rizki Karyanto <mirk@dr.com>
2018-03-11 11:44:17 +0700

    Update.

---
+++ app/web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GamaVision</title>
+</head>
+<body>
+	<form>
+		<input type="text" name="username" placeholder="Username">
+		<input type="password" name="password" placeholder="Password">
+		<button>Login</button>
+	</form>
+</body>
+</html>
